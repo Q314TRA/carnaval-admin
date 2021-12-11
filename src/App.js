b1cef99e7331d02a60e62dc517296ede44d55cb8
Luis Miguel Cupitra Vergara <luismiguelcupitravergara@Luiss-MacBook-Air.local>
2021-12-11 00:57:38 -0500

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import { Container, Row, Col, Button, InputGroup, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import olmes from './olmes.webp';
+import axios from 'axios';
 
 function App() {
+  const [phone, setPhone] = useState('');
+  const [users, setUsers] = useState([]);
+  const [usersCount, setUsersCount] = useState(0);
+  let findByPhone = () => {
+    axios.get(`http://localhost:3000/carnaval?phone=${phone}`)
+      .then(item => setUsers(item.data))
+  }
+
+  let updateCheck = (id) => {
+    axios.put(`http://localhost:3000/carnaval`, { id })
+      .then(findByPhone)
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/carnavalcoutn`)
+      .then(item => setUsersCount(item.data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" fluid>
+      <Row fluid className='header mb-5' style={{ backgroundImage: `url(${olmes})` }} >
+        <div className='fade-content'>
+          <div className='fade-circle'>
+            <span>{usersCount}</span>
+          </div>
+        </div>
+      </Row>
+      <Row className='main mb-5'>
+        <Col xs={10}>
+          <InputGroup >
+            <FormControl type="tel" placeholder="Ingrese el telefono"
+              aria-label="Ingrese el telefono" onChange={(el) => setPhone(el.target.value)} />
+            <Button variant="outline-secondary" onClick={findByPhone}>
+              Buscar
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row className='search-content'>
+        {users.map(item =>
+          <Col xs={10} className='mb-3' >
+            <h5>{item.name}</h5>
+            <p>
+              Estado: <span>{item.checked ? "Validado" : "Pendiente"}</span> <br />
+              Codigo: <span>{item._id}</span>
+            </p>
+            {!item.checked && <Button variant="outline-secondary" onClick={() => updateCheck(item._id)}>
+              Validar
+            </Button>}
+
+          </Col>
+        )}
+
+      </Row>
+
+    </Container>
   );
 }
 
